15a92f032debf933c98b3665df86e24128cf3190
QingyunWu <wuhaiwqy@hotmail.com>
2026-01-20 15:07:09 +0800

    add ShowNotification to View

--- Framework.Common/Mvvm/UniViewModel.cs
+++ Framework.Common/Mvvm/UniViewModel.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
 using Avalonia.Interactivity;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -7,6 +8,30 @@ namespace Framework.Common;
 public class UniViewModel : ObservableObject
 {
     public Control? View { get; set; }
+
+    public virtual void OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        TopLevel? topLevel = TopLevel.GetTopLevel(View);
+        if (_lastTopLevel != topLevel)
+        {
+            _lastTopLevel = topLevel;
+            _notificationManager = new WindowNotificationManager(TopLevel.GetTopLevel(View))
+            {
+                Position = NotificationPosition.TopCenter,
+                MaxItems = 1
+            };
+        }
+    }
     
-    public virtual void OnLoaded(object? sender, RoutedEventArgs e) { }
+    protected void ShowNotification(string message, NotificationType notificationType)
+    {
+        Notification notification = new()
+        {
+            Message = ResourceHelper.FindStringResource(message)
+        };
+        _notificationManager?.Show(notification, type: notificationType);
+    }
+    
+    private WindowNotificationManager? _notificationManager;
+    private TopLevel? _lastTopLevel;
 }
