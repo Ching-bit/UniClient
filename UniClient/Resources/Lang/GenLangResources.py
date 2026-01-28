3c5f0fb08413a78a272303f453d10e70781ba8f8
QingyunWu <wuhaiwqy@hotmail.com>
2026-01-28 20:52:25 +0800

    fix bugs

--- UniClient/Resources/Lang/GenLangResources.py
+++ UniClient/Resources/Lang/GenLangResources.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             with open(file_name, "w", encoding="utf-8") as f:
                 f.write('<ResourceDictionary\n    xmlns="https://github.com/avaloniaui"\n    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"\n    xmlns:system="clr-namespace:System;assembly=System.Runtime">\n\n')
                 for row in reader:
-                    if len(row) <= len(header):
+                    if len(row) != len(header):
                         continue
                     key = row[0]
                     value = row[i]
